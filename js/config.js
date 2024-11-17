7450aa38e09b151ebf7188589bf86a02cc96cc17
viviana222601 <86531853+viviana222601@users.noreply.github.com>
2024-11-17 18:31:39 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1_lJv3JPCFi' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://bz2fqctg46.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
